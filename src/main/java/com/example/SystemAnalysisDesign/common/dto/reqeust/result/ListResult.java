fa1b993de45d64aace2f79ae47061382091c6109
shash0423 <shash042319@naver.com>
2024-11-26 12:58:33 +0900

    feat : 응답 규격화, 에러 핸들링

---
+++ src/main/java/com/example/SystemAnalysisDesign/common/dto/reqeust/result/ListResult.java
@@ -0,0 +1,15 @@
+package com.example.SystemAnalysisDesign.common.dto.reqeust.result;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class ListResult<T> {
+
+    @Schema(description = "리스트 데이터")
+    private List<T> list;
+}
---
+++ src/main/java/com/example/SystemAnalysisDesign/common/service/ResponseService.java
@@ -0,0 +1,25 @@
+package com.example.SystemAnalysisDesign.common.service;
+
+import com.example.SystemAnalysisDesign.common.dto.reqeust.result.ListResult;
+import com.example.SystemAnalysisDesign.common.dto.reqeust.result.SingleResult;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ResponseService {
+
+    // 단일 값을 감싸는 메서드
+    public static <T> SingleResult<T> getSingleResult(T data) {
+        SingleResult<T> result = new SingleResult<>();
+        result.setData(data);
+        return result;
+    }
+
+    // 리스트 값을 감싸는 메서드
+    public static <T> ListResult<T> getListResult(List<T> list) {
+        ListResult<T> result = new ListResult<>();
+        result.setList(list);
+        return result;
+    }
+}
