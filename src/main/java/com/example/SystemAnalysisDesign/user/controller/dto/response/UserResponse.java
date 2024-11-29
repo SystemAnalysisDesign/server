dfc6a6dbb021eca9d0ecd9634057dfbae502f15e
shash0423 <shash042319@naver.com>
2024-11-29 11:02:37 +0900

    fix : UserResponse에 keywords 필드 추가

--- src/main/java/com/example/SystemAnalysisDesign/user/controller/dto/response/UserResponse.java
+++ src/main/java/com/example/SystemAnalysisDesign/user/controller/dto/response/UserResponse.java
@@ -4,17 +4,25 @@ import com.example.SystemAnalysisDesign.user.domain.User;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 @Builder
 public class UserResponse {
 
     private Long id;
     private String email;
+    private List<String> keywords;
 
     public static UserResponse from(User user) {
+        List<String> keywordNames = user.getUserKeywords().stream()
+                .map(userKeyword -> userKeyword.getKeyword().getName())
+                .toList();
+
         return UserResponse.builder()
                 .id(user.getId())
                 .email(user.getEmail())
+                .keywords(keywordNames)
                 .build();
     }
 }
